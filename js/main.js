268fe5f4bc7334425b3bd158689ad615abd1d7a5
Julian Boaler <julianboaler111@gmail.com>
2020-07-13 01:16:16 -0600

    Added more to ajax query

--- js/main.js
+++ js/main.js
@@ -1,27 +1,35 @@
 $(document).ready(()=> {
     console.log( "ready!" );
 
-
-
     $('#submit').click(()=>{
         let userInput = $('#search').val()
         var url = "http://api.giphy.com/v1/gifs/search?q="+userInput+"&api_key=voUaBLOKvSwR1AKRieHkVGy30tkiCzCn"
-        var data = "";
-        alert(userInput)
+        var data = $('.inputdata').serializeArray();
         
         
-        $.ajax({
-        type: "GET",
-        url:url,
-        data:data,
-        success: function(data){
-            console.log("success " +data);
+        $.ajax( {
+            data: data,
+            type: "GET",
+            url: url,
+            success: function(data) {
+                console.log("success " +data);
+                var outputData = "";
+                $.each( data.data, function( key, obj) {
+                    /* if (obj.id !=undefined) {
+                        cnt++
+                        var images="https://media1.giphy.com/media"+obj.id+"/giphy"+obj.type+
+                    } 
+                    */
+                    var gifUrl = obj._url;
+                    console.log("url= "+gifUrl);
+                    outputData += "<span><img src='"+gifUrl+"'></span>"
+                });
+                $('.gifoutput').html(outputData);
             }
         
         });
 
-        var giphydata = $('.inputData').serialize();
-        console.log(giphydata);
+        
         
             
     });
